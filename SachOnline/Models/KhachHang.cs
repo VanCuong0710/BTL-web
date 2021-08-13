26f7705aca7a0e83ea0c7b381114f9855034bd62
VanCuong0710 <77467606+VanCuong0710@users.noreply.github.com>
2021-08-14 02:59:09 +0700

    abc

--- SachOnline/Models/KhachHang.cs
+++ SachOnline/Models/KhachHang.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -17,21 +18,26 @@
 
         [Key]
         [StringLength(10)]
+        [DisplayName("SĐT: ")]
         [Required(ErrorMessage ="Trường này không được để trống")]
         public string SDT { get; set; }
 
+        [DisplayName("Tên: ")]
         [StringLength(50)]
         [Required(ErrorMessage = "Trường này không được để trống")]
         public string TenKH { get; set; }
 
+        [DisplayName("Email: ")]
         [StringLength(50)]
         [Required(ErrorMessage = "Trường này không được để trống")]
         public string Email { get; set; }
 
+        [DisplayName("Mật khẩu: ")]
         [StringLength(50)]
         [Required(ErrorMessage = "Trường này không được để trống")]
         public string MatKhau { get; set; }
 
+        [DisplayName("Địa chỉ: ")]
         [StringLength(50)]
         [Required(ErrorMessage = "Trường này không được để trống")]
         public string DiaChi { get; set; }
